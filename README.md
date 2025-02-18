078c727e14e77e978c0442ec98eb4ab1fd74339a
jonas <jonas.haegd@gmail.com>
2025-02-18 20:33:27 +0100

    Update logic and readme

--- README.md
+++ README.md
@@ -3,12 +3,11 @@ Archipelago tracker for Pokemon Mystery Dungeon: Explorers of Sky
 
 Current issues:
 - Bad or missing images
-- Some logic does not update until an item is recieved (Clearing Beach Cave, Temporal Tower, Surrounded Sea and Miracle Sea)
-	- This is so that the player does not think they have an item before they beat a dungeon in another place
 
 ## NOTE
 
 This tracker is not made for manual usage, please be warned that missions and outlaws will not be fun to track manually.
+Events are also currently not checkable in manual mode, and will only be toggled with an Archipelago connection.
 
 # Example image
 
--- scripts/logic.lua
+++ scripts/logic.lua
@@ -23,11 +23,6 @@ function darkraiGoal()
 	return count > 0
 end
 
-function canClearTemporalTower()
-	local tower = Tracker:ProviderCountForCode("Temporal Tower")
-	return enoughRelicFragments() and tower == 1
-end
-
 function enoughRelicFragments()
 	local relicCount = Tracker:ProviderCountForCode("RelicFragmentCount")
 	local relicGoal = Tracker:ProviderCountForCode("RequiredRelicFragmentShards")
@@ -36,23 +31,9 @@ end
 
 function canAccessDarkCrater()
 	if darkraiGoal() then
-		local tower = Tracker:FindObjectForCode("@Dungeons/Temporal Tower/Complete Temporal Tower")
 		local instrumentCount = Tracker:ProviderCountForCode("Instruments")
 		local instrumentGoal = Tracker:ProviderCountForCode("RequiredInstruments")
 		return instrumentCount >= instrumentGoal and tower.AvailableChestCount == 0
 	end
 	return Tracker:ProviderCountForCode("Dark Crater") == 1
-end
-
-function dungeonCleared(dungeonName)
-	local dungeon = Tracker:FindObjectForCode("@Dungeons/"..dungeonName.."/Complete "..dungeonName)
-	return dungeon.AvailableChestCount == 0
-end
-
-function has(item)
-    return Tracker:ProviderCountForCode(item) == 1
-end
-
-function completed(location)
-	return Tracker:FindObjectForCode('@Dungeons\\' + location + '\\Complete ' + location).ChestCount == 1
 end
